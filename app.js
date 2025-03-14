28abc5db5eed3130595c0e1790774c9edfb4caba
biizerrah <biizerrah@outlook.com>
2025-03-14 20:15:25 -0300

    Criando a função de adicionar um amigo

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = []
+
+
+function adicionarAmigo(){
+
+    let mostrarAmigos = document.getElementById("listaAmigos")
+    mostrarAmigos.innerHTML = ""
+    let nome = document.getElementById("amigo").value
+
+    amigos.push(nome)
+    document.getElementById("amigo").value = " "
+    
+
+    for(let i = 0; i < amigos.length; i++){
+
+    mostrarAmigos.innerHTML += `<li>${amigos[i]}</li>`
+    
+}
+}
+
